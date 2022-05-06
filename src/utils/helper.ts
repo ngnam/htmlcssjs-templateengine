13deefd58ee989ca702e562ce3e70e249a4c57ec
ngnam <nguyenvannam0411@gmail.com>
2022-05-06 17:01:54 +0700

    commit

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -55,7 +55,73 @@ async function pasteFromClipboard(): Promise<string | undefined> {
     }
 }
 
+/**
+ * How to split a string in JavaScript
+ */
+/**
+ * Split a string into substrings using the specified separator and return them as an array.
+ * @param word
+ * @param separator — A string that identifies character or characters to use in separating the string. If omitted, a single-element array containing the entire string is returned.
+ * @param limit — A value used to limit the number of elements returned in the array.
+ * @returns string[]
+ */
+function splitAStringWord(word: string, separator: string | RegExp, limit?: number | undefined): string[] {
+    return word.split(separator, limit);
+}
+
+/**
+ * How to parse JSON in JavaScript
+ * Converts a JavaScript Object Notation (JSON) string into an object.
+ * @param text — A valid JSON string.
+ * @param reviver
+ * A function that transforms the results. This function is called for each member of the object. If a member contains nested objects, the nested objects are transformed before the parent object is.
+ */
+function pasteToJSON(text: string, reviver?: ((this: any, key: string, value: any) => any) | undefined): any {
+    try {
+        return JSON.parse(text, reviver);
+    } catch (error) {
+        console.log(`JSON invalid::[${text}]`, error);
+        return null;        
+    }
+}
+
+/**
+ * 
+ * @param json object
+ * @returns 
+ */
+function pasteFromJson(json: any): string {
+    return JSON.stringify(json);
+}
+
+function create2DArray(rows: number, cols: number, length: number) {
+    let arrRender = Array.from(Array(length).keys());
+    let arr = new Array(rows);
+    let index = 0;
+    let slice_next = rows;
+    // 5, 10, 15, 20, 25 -- slice_next
+    // 0,  1   2   3   4 -- i
+
+    // x
+    for (var i = 0; i < rows; i++) {
+        arr[index] = arrRender.slice(index, slice_next); // new Array(cols);
+        index = slice_next;
+        slice_next += rows;
+    }
+
+    // y
+    for (let j = 0; j < cols; j++) {
+        
+    }
+
+    return arr.filter(Boolean);
+  }
+
 export default {
     copyToClipboard,
-    pasteFromClipboard
+    pasteFromClipboard,
+    splitAStringWord,
+    pasteToJSON,
+    pasteFromJson,
+    create2DArray
 }
