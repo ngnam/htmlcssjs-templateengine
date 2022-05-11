bda9dd722b8d444c2547f92047631f29ae8188d5
ngnam <nguyenvannam0411@gmail.com>
2022-05-11 23:19:29 +0700

    new branch start

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,25 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
+import Footer from '../components/Footer';
 import { MiniAppRouter } from '../routers/mini-app.route'
+import Navbar from './../components/Navbar';
 
 const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2 ">
       <Head>
-        <title>5 Mini web apps | 5 Web apps template</title>
+        <title>100+ Mini Application - ngnam.github.io</title>
+        <meta name="description" content="100+ Mini Application, using ReactJS + Typescript, Taiwindcss Framework & Others" />
         <link rel="icon" href="/favicon.ic  o" />
       </Head>
 
+      <Navbar />
+
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-6xl font-bold">
-          5 Mini web apps
+          <b>100+ Mini Application</b> <br /> 
+          <small>using ReactJS + Typescript + Taiwindcss Framework + Others.</small>
         </h1>
 
         <p className="mt-3 text-2xl">
@@ -39,6 +45,7 @@ const Home: NextPage = () => {
         </div>
       </main>
 
+      <Footer />
       <footer className="flex h-24 w-full items-center justify-center border-t">
         <div className="flex items-center justify-center gap-2">
           Powered by <Link rel="noopener noreferrer" href="https://ngnam.github.io" target="_blank"><a>https://ngnam.github.io</a></Link>
